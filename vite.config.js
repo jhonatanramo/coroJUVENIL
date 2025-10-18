0809d92a05a8d4eecd511efaa9d953fe4de7275f
jhonatanramo <jhonatanleonel666@gmai,com>
2025-10-18 02:52:17 -0400

    json audate

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Coro Juvenil',
+        short_name: 'Coro',
+        description: 'Aplicación del Coro Juvenil',
+        theme_color: '#2A3A71',
+        background_color: '#96D1F1',
+        display: 'standalone', // <-- clave para pantalla completa
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
